678e60f42abeff89a135eb9638d41085ad6b3a2e
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-23 22:49:47 +0300

    str method

--- password.py
+++ password.py
@@ -2,3 +2,13 @@ class User:
     userInfo = list()
     def __init__(self, userName, userPassword):
         self.userName, self.userPassword = userName, userPassword
+    def setUserName(self, userName):
+        self.userName = userName
+    def getUserName(self):
+        return self.userName
+    def setUserPassword(self, userPassword):
+        self.userPassword = userPassword
+    def getUserPaswword(self):
+        return self.userPaswword
+    def __str__(self):
+        return ("%s %d"%())  
