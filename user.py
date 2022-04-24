232b084be3c91c62c555d12bd18b9f11aba81093
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-24 12:15:24 +0300

    add user

--- user.py
+++ user.py
@@ -2,3 +2,17 @@ class User:
     """
     generate new instances of users
     """
+    userslist=[]
+    def __init__(self, firstname, lastname, username, password):
+        """
+        method to define properties for objects
+        """
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = password
+    def save_user(self):
+        """
+        method saves user into users list
+        """  
+        User.userslist.append(self)
