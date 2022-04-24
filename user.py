4df345b383b65d7f4fd5adc1220803c568a5e165
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-24 12:24:22 +0300

    used the classmethod

--- user.py
+++ user.py
@@ -16,3 +16,21 @@ class User:
         method saves user into users list
         """  
         User.userslist.append(self)
+    def delete_user(self):
+        """
+        removes user from list
+        """
+        User.userslist.remove(self)
+
+    @classmethod
+    def display_users(cls):
+        """
+        return info from users
+        """
+        return cls.userslist
+
+    @classmethod
+    def find_by_number(cls, number):
+        """
+        
+        """    
