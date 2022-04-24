a6a220036233cd94872dfe051cd262923bfe95da
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-04-24 13:30:21 +0300

    file to run program

--- credentials.py
+++ credentials.py
@@ -1,2 +1,30 @@
 class Credentials:
-    
+    accounts=[]
+
+    def __init__(self, accountusername, accountname, accountpassword):
+        """
+        defines properties for objects
+        """
+        self.accountusername = accountusername
+        self.accountname = accountname
+        self.accountpassword = accountpassword
+
+    def save_account(self):
+        Credentials.accounts.append(self)
+
+    def delete_account(self):
+        Credentials.accounts.remove(self)
+
+    @classmethod
+    def display_aacounts(cls):
+        """
+        list of accounts
+        """
+        for account in cls.accounts:
+            return cls.accounts
+
+    @classmethod
+    def find_by_number(cls,number):
+        for account in cls.accounts:    
+              if account.accountsusername == number:
+                  return account
